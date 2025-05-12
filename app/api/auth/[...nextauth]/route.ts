d9899f6c7a344ceebb6388dbc2f55962ab4b81e1
Rishi2600 <rishiraj6177@gmail.com>
2025-05-12 19:46:11 +0530

    added env variables

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        }),
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID ?? "",
             clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ""
